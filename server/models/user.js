ebc75625e7d811e7b9697a6db4c96e7267b30008
Harpreet Singh <singhharpreet.chadha@gmail.com>
2017-12-09 11:43:54 +0530

    add Post Todo route , refactor the mongoose and also used body parser

---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+var User = mongoose.model('User',{
+  email : {
+    required : true,
+    trim : true,
+    type : String,
+    minlength : 1
+  }
+})
+
+module.exports ={User}
