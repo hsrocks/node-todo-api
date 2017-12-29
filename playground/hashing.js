8bdb554dbcd28a0d37b784aad69c8392aab32211
Harpreet Singh <singhharpreet.chadha@gmail.com>
2017-12-30 00:04:58 +0530

    hashed salt the passwork using bcryptjs

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,35 +1,49 @@
-// var {SHA256} = require("crypto-js");
+// // var {SHA256} = require("crypto-js");
+// //
+// // var message = 'I am User Number 1';
+// //
+// // var hash= SHA256(message).toString();
+// //
+// // console.log(hash);
+// //
+// // var data ={
+// //   id : 3
+// // };
+// //
+// // var token={
+// //   data,
+// //   hash : (SHA256(JSON.stringify(data))+'someSecret').toString()
+// // }
+// //
+// // var resultHash =(SHA256(JSON.stringify(token.data))+'someSecret').toString();
+// //
+// // if(token.hash === resultHash){
+// //   console.log('Data was not changed');
+// // }
+// // else {
+// //   console.log('Data was changed');
+// // }
+// const jwt = require('jsonwebtoken');
 //
-// var message = 'I am User Number 1';
-//
-// var hash= SHA256(message).toString();
-//
-// console.log(hash);
-//
-// var data ={
-//   id : 3
-// };
-//
-// var token={
-//   data,
-//   hash : (SHA256(JSON.stringify(data))+'someSecret').toString()
+// var data = {
+//   id : 10
 // }
+// var token = jwt.sign(data,'123abc');
+// console.log(token)
 //
-// var resultHash =(SHA256(JSON.stringify(token.data))+'someSecret').toString();
-//
-// if(token.hash === resultHash){
-//   console.log('Data was not changed');
-// }
-// else {
-//   console.log('Data was changed');
-// }
-const jwt = require('jsonwebtoken');
+// var decodedResult = jwt.verify(token,'123abc');
+// console.log(decodedResult);
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id : 10
-}
-var token = jwt.sign(data,'123abc');
-console.log(token)
+ var password = "1223abc!";
+// bcrypt.genSalt(1,(err,salt)=>{
+//   bcrypt.hash(password,salt,(err,hash)=>{
+//     console.log(hash);
+//   })
+// })
 
-var decodedResult = jwt.verify(token,'123abc');
-console.log(decodedResult);
+
+var password1 = "$2a$04$6sZsndOO9FKkh1CoSxNqnO80o7wsjXpLpAN9jLu3XYxmLhTcjwQV."
+bcrypt.compare(password,password1,(err,res)=>{
+  console.log(res);
+})
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 var validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 // var User = mongoose.model('User',{
 //   email : {
 //     required : true,
@@ -107,6 +108,21 @@ UserSchema.statics.findByToken = function(token){
 
 };
 
+UserSchema.pre('save',function(next){
+  var user= this;
+  if(user.isModified('password')){
+    bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(user.password,salt,(err,hash)=>{
+        user.password=hash;
+        next();
+      })
+    })
+  }
+  else{
+    next();
+  }
+
+})
 var User = mongoose.model('User',UserSchema);
 
 module.exports ={User}
