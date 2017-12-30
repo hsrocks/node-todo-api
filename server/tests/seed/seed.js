b149e4857a29f4ff1b75059e33e503695fefab0d
Harpreet Singh <singhharpreet.chadha@gmail.com>
2017-12-30 13:47:11 +0530

    add user test POST /users GET /users/me and seed the user data in test database

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,57 @@
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users =[{
+  _id : userOneId,
+  email : 'hs@example.com',
+  password : 'userOnePass',
+  tokens:[{
+    access : 'auth',
+    token : jwt.sign({
+      _id : userOneId.toHexString(),
+      access : 'auth'
+    },'abc123').toString()
+  }]
+},{
+  _id : userTwoId,
+  email : 'my@example.com',
+  password : 'userTwoPass'
+}];
+
+const todos=[{
+  _id : new ObjectID(),
+  text : "Hello"
+},{
+  _id : new ObjectID(),
+  text : "Hi",
+  completed : true,
+  completedAt : 333
+}];
+
+const populateTodos = (done) =>{
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=> done());
+}
+
+const populateUsers = (done) => {
+    User.remove({}).then(()=>{
+      var userOne= new User(users[0]).save();
+      var userTwo= new User(users[1]).save();
+
+      // I have returned promise so that I can call then to complete the populateUsers
+      return Promise.all([userOne, userTwo])
+    }).then(()=>done());
+};
+
+module.exports ={
+  todos,
+  populateTodos,
+  users,
+  populateUsers
+}
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,22 +2,13 @@ const expect = require('expect');
 const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
 const {ObjectID} = require('mongodb')
-const todos=[{
-  _id : new ObjectID(),
-  text : "Hello"
-},{
-  _id : new ObjectID(),
-  text : "Hi",
-  completed : true,
-  completedAt : 333
-}];
 
-beforeEach((done) =>{
-  Todo.remove({}).then(()=>{
-    return Todo.insertMany(todos);
-  }).then(()=> done());
-});
+const {todos,populateTodos,users,populateUsers} = require('./seed/seed');
+
+beforeEach(populateUsers)
+beforeEach(populateTodos);
 
 describe('POST /todos',()=>{
   it('should create a new todo',(done)=>{
@@ -179,5 +170,82 @@ describe('Patch todo/:id',()=>{
     expect(res.body.todo.completedAt).toNotExist();
   }).
   end(done)
+  })
+})
+
+
+describe('GET /users/me',()=>{
+  it('should return user if authenticated',(done)=>{
+    request(app).get('/users/me').
+    set('x-auth',users[0].tokens[0].token).
+    expect(200).
+    expect((res)=>{
+      expect(res.body._id).toBe(users[0]._id.toHexString());
+      expect(res.body.email).toBe(users[0].email);
+    }).
+    end(done)
+  });
+
+  it('should return 401 if not authenticated',(done)=>{
+    request(app).get('/users/me').
+    expect(401).
+    expect((res)=>{
+      expect(res.body).toEqual({});
+    }).
+    end(done)
+  });
+
+  it('It should return 401 with wrong jwt signature', (done) => {
+    var token = users[0].tokens[0].token.slice(0, -1) + 'Z';
+    request(app)
+        .get('/users/me')
+        .set({'x-auth': token})
+        .expect(401)
+        .end(done);
+});
+});
+
+describe('POST /users',()=>{
+  it('should create a User',(done)=>{
+    var email ='example@example.com'
+    var password ='abc123!';
+    request(app).post('/users').
+    send({email,password}).
+    expect(200).
+    expect((res)=>{
+      expect(res.headers['x-auth']).toExist();
+      expect(res.body._id).toExist();
+      expect(res.body.email).toBe(email);
+    }).
+    end((err)=>{
+      if(err){
+        return done(err)
+      }
+        User.findOne({email}).then((user)=>{
+          expect(user).toExist();
+          expect(user.password).toNotBe(password);
+          done();
+        })
+    });
+  })
+
+  it('should return validation error if request invalid',(done)=>{
+    var email="hs";
+    var password="as";
+    request(app).post('/users').send({
+      email,
+      password
+    }).expect(400).
+    end(done);
+  })
+
+  it('should not create a new user if email is in used',(done)=>{
+    var email="hs@example.com";
+    var password="as123344";
+    request(app).post('/users').send({
+      email,
+      password
+    }).expect(400).
+    end(done);
   })
 })
